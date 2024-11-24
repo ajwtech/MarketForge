054f21243fda3894d1858f9fd96d98dcf380857b
Adam Wheeler <ajwtech@gmail.com>
2024-11-24 00:16:11 -0500

    Refactor Pulumi configuration and infrastructure setup for improved clarity and organization

---
+++ azure-deploy/infra/infrastructure/resourceGroup.ts
@@ -0,0 +1,14 @@
+
+import * as pulumi from "@pulumi/pulumi";
+import * as azure_native from "@pulumi/azure-native";
+
+const config = new pulumi.Config();
+const location = config.require("location");
+const resourceGroupName = config.require("resourceGroupName");
+
+export const ResourceGroup = new azure_native.resources.ResourceGroup(resourceGroupName, {
+    location: location,
+    resourceGroupName: resourceGroupName, // Ensure this is set in config
+}, {
+    protect: false,
+});
