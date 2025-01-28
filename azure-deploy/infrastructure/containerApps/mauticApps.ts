6ec0287247a70c2d8d70a77addd9ecb0969c9d07
Adam <ajwtech@gmail.com>
2025-01-28 13:40:07 -0500

    Remove Mautic init, cron, and worker scripts; update Docker configurations accordingly

--- azure-deploy/infrastructure/containerApps/mauticApps.ts
+++ azure-deploy/infrastructure/containerApps/mauticApps.ts
@@ -1,7 +1,5 @@
 import { mauticWeb } from "./mauticWeb";
-import { mauticCron } from "./mauticCron";
-import { mauticWorker } from "./mauticWorker";
 import { mauticNginx } from "./mauticNginx";
 
-export { mauticWeb, mauticCron, mauticWorker, mauticNginx };
+export { mauticWeb, mauticNginx };
 
--- azure-deploy/infrastructure/dockerImages.ts
+++ azure-deploy/infrastructure/dockerImages.ts
@@ -1,40 +1,14 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as dockerbuild from '@pulumi/docker-build';
 import { marketingcr, acrUsername, acrPassword, registryUrl } from './registries/acrRegistry';
-import * as command  from "@pulumi/command";
 
 const config = new pulumi.Config();
 const imageTag = config.get('imageTag') || 'latest';
 const imageNames = [
   'marketing-nginx',
   'marketing-mautic_web',
-  ////uncomment the following lines to build the images for the marketing-mautic_cron and marketing-mautic_worker
-  // 'marketing-mautic_cron',
-  // 'marketing-mautic_worker',
 ];
-const pushImages = config.get('pushImages') || 'false'
 const imageBuilds: { [key: string]: dockerbuild.Image } = {};
-
-// Use Azure CLI to check if the image exists in ACR
-function imageExists(acrName: string, imageName: string ): pulumi.Output<boolean> {
-    try {
-      if (pushImages === 'true') {
-        return pulumi.output(true);
-    }
-        const imageExistsCommand = command.local.runOutput({
-            command: pulumi.interpolate`az acr repository show-tags --name ${acrName} --repository ${imageName} -o json || echo []`,
-        });
-        const output =  imageExistsCommand.stdout.apply(stdout => {
-            const tags = JSON.parse(stdout);
-            
-            return tags.includes(imageTag);
-        });
-        return output;
-    } catch (error) {
-        return pulumi.output(false);
-    }
-}
-
 
 // Wait for the ACR to be created before proceeding
 marketingcr.name.apply(acrName => {
