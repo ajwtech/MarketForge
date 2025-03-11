18bf6892477bbf824fd9ceb7c476b870133de9b7
Adam <ajwtech@gmail.com>
2025-03-11 14:02:19 -0400

    Update Strapi configuration to use the AWS S3 integration to work with the Azure storage. the Azure storage plugin is not maintained and the AWS one is setup to work for both; add public and private asset containers, and update dependencies

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -1,1 +1,35 @@
-export default () => ({});
+export default () => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: process.env.STORAGE_ACCOUNT_NAME,
+        secretAccessKey: process.env.STORAGE_ACCOUNT_KEY,
+        region: 'us-east-1', // Required by AWS SDK but not used by Azure
+        endpoint: `https://${process.env.STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
+        s3ForcePathStyle: true, 
+        signatureVersion: 'v4', // Add signature version for compatibility
+        params: {
+          Bucket: 'assets',
+          ACL: 'public-read',
+        },
+      },
+      actionOptions: {
+        upload: {
+          ACL: 'public-read' // Ensure uploads are publicly readable
+        },
+        uploadStream: {
+          ACL: 'public-read'
+        },
+        delete: {},
+      },
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
+      },
+    },
+  },
+});
