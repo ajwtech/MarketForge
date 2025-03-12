7f4ef2651cad682968fed11057d55b479eef0fc9
Adam <ajwtech@gmail.com>
2025-03-12 14:35:06 -0400

    Update middleware configuration to use the correct variable, STORAGE_ACCOUNT_NAME, for CSP headers

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -12,12 +12,11 @@ export default [
             "'self'",
             "data:",
             "blob:",
-            "https://market-assets.strapi.io", // Required for Strapi >= 4.10.6, you can remove it otherwise
             /**
              * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
              * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
              */
-            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+            `https://${process.env.STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
           ],
           "media-src": [
             "'self'",
@@ -27,7 +26,7 @@ export default [
              * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
              * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
              */
-            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+            `https://${process.env.STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
 
           ],
           upgradeInsecureRequests: null,
