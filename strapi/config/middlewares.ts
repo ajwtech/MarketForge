c47511bb5dfb6fa9cdc262912d72b59fb44b321b
Adam <ajwtech@gmail.com>
2025-03-12 21:21:03 -0400

    Add market-assets.strapi.io to CSP headers in middleware configuration despite the plugins sugesstion this is still needed for strapi v5

--- strapi/config/middlewares.ts
+++ strapi/config/middlewares.ts
@@ -12,6 +12,7 @@ export default [
             "'self'",
             "data:",
             "blob:",
+            "https://market-assets.strapi.io",
             /**
              * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
              * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
@@ -22,6 +23,7 @@ export default [
             "'self'",
             "data:",
             "blob:",
+            "https://market-assets.strapi.io",
             /**
              * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
              * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
