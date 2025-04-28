a5834868ddcfaccdb929e090c1112b07c1ea6e7a
Adam <ajwtech@gmail.com>
2025-04-28 14:00:43 -0400

    feat: add logging workspace integration to managed environment configuration

---
+++ azure-deploy/infrastructure/analytics/logging.ts
@@ -0,0 +1,19 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as azure_native from "@pulumi/azure-native";
+import { ResourceGroup } from "../resourceGroup";
+
+
+const config = new pulumi.Config();
+const location = config.require("location");
+const resourceGroupName = ResourceGroup.name;
+const logAnalyticsWorkspaceId = config.require("logAnalyticsWorkspaceId");
+
+export const logAnalyticsWorkspace = new azure_native.operationalinsights.v20230901.Workspace(logAnalyticsWorkspaceId, {
+    location: location,
+    resourceGroupName: resourceGroupName,
+    sku: {
+        name: "PerGB2018",
+    },
+    retentionInDays: 30
+    
+});
