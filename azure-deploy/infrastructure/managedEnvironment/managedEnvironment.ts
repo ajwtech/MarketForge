641b1baf16a2e2b6b225aaee0a1da565a76ad208
Adam <ajwtech@gmail.com>
2025-04-28 14:00:43 -0400

    feat: add logging workspace integration to managed environment configuration

--- azure-deploy/infrastructure/managedEnvironment/managedEnvironment.ts
+++ azure-deploy/infrastructure/managedEnvironment/managedEnvironment.ts
@@ -2,11 +2,20 @@ import * as pulumi from "@pulumi/pulumi";
 import { v20241002preview as azure_app } from "@pulumi/azure-native/app";
 import { ResourceGroup } from "../resourceGroup";
 import { subnet } from "../networking/subnet";
-
+import { logAnalyticsWorkspace } from "../analytics/logging";
+import * as azure_native from "@pulumi/azure-native";
 
 const config = new pulumi.Config();
 const location = config.require("location");
 const resourceGroupName = ResourceGroup.name;
+const sharedKey = pulumi
+    .all([logAnalyticsWorkspace.name, resourceGroupName])
+    .apply(([workspaceName, resourceGroupName]) =>
+        azure_native.operationalinsights.getWorkspaceSharedKeys({
+            workspaceName: workspaceName,
+            resourceGroupName: resourceGroupName,
+        }).then(keys => keys.primarySharedKey ?? "")
+    );
 
 // Create Managed Environment
 export const marketing_env = new azure_app.ManagedEnvironment("marketing-env", {
@@ -17,6 +26,14 @@ export const marketing_env = new azure_app.ManagedEnvironment("marketing-env", {
         infrastructureSubnetId: subnet.id,
         internal: false,     
     },
+    appLogsConfiguration: {
+        destination: "log-analytics",
+        logAnalyticsConfiguration: {
+            customerId: logAnalyticsWorkspace.customerId,
+            sharedKey: sharedKey,
+        },
+    },
+
     
     zoneRedundant: false,
 }, {
