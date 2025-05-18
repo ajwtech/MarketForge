e127f09ff8f97ce4b0dd9cba9f644a0f9da1dbb2
Adam Wheeler <ajwtech@gmail.com>
2025-05-17 23:56:15 -0400

    Add ACR credentials to Docker registry environment variables setup

--- azure-deploy/infrastructure/dockerImages.ts
+++ azure-deploy/infrastructure/dockerImages.ts
@@ -1,6 +1,6 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as docker from '@pulumi/docker';
-import { registryUrl } from './registries/acrRegistry';
+import { registryUrl, acrUsername, acrPassword } from './registries/acrRegistry';
 
 const config = new pulumi.Config();
 const imageTag = config.get('imageTag') || 'latest';
@@ -13,6 +13,17 @@ const imageNames = [
 ];
 
 const imageBuilds: { [key: string]: pulumi.Output<string> } = {};
+
+// Set Docker registry environment variables dynamically at runtime
+registryUrl.apply(url => {
+  process.env.DOCKER_SERVER = url;
+});
+acrUsername.apply(username => {
+  process.env.DOCKER_USERNAME = username;
+});
+acrPassword.apply(password => {
+  process.env.DOCKER_PASSWORD = password;
+});
 
 for (const imageName of imageNames) {
   // The full remote tag (with registry name)
