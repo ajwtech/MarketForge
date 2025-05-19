8901d03db7c23c7acff7273ad2dbad30c09224dc
Adam Wheeler <ajwtech@gmail.com>
2025-05-18 21:47:56 -0400

    Overhaul github action and pulumi deployment. used actions where available and moved away from run commands as much as possible. Split the pulumi runs so that the containers are built and uploaded in github rather than in github and rebuilt and uploaded in pulumi

--- azure-deploy/index.ts
+++ azure-deploy/index.ts
@@ -118,6 +118,275 @@ function getImageName(imageName: string): pulumi.Output<string> {
     return registryUrl.apply(url => `${url}/${imageName}:${imageTag}`);
 }
 
+// Remove all app deployment from global context. Only define in setup-apps/default cases.
+const githubJob = process.env.GITHUB_JOB;
+
+switch (githubJob) {
+  case "setup-acr-infra": {
+    // Only create ACR and its dependencies, and output creds
+    // ResourceGroup and ACR registry are always needed
+    // Export acrUsername, acrPassword, registryUrl
+    // (Assume these are imported and defined at the top)
+    export const acrUsernameOut = acrUsername;
+    export const acrPasswordOut = acrPassword;
+    export const registryUrlOut = registryUrl;
+    break;
+  }
+  case "setup-infra": {
+    // Create all remaining infrastructure except container apps
+    // This includes storage, managed environment, databases, etc.
+    // Do not create container apps or build/push images here
+    // Create storage configuration in the managed environment for Mautic (uses marketingstacksa)
+const mauticStorage = new azure_app.ManagedEnvironmentsStorage("mautic-app-files-storage", {
+    environmentName: marketing_env.name,
+    resourceGroupName: resourceGroupName,
+    properties: {
+        azureFile: {
+            accountName: storageAccountName,
+            shareName: mauticAppFilesStorage.name, // e.g. "mautic-app-files"
+            accessMode: "ReadWrite",
+            accountKey: storageAccountKey,
+        },
+    }, 
+}, {
+    protect: false,
+    dependsOn: [mauticAppFilesStorage],
+});
+
+// Create dedicated storage for Strapi
+const strapiStorage = new azure_app.ManagedEnvironmentsStorage("strapi-app-files-storage", {
+    environmentName: marketing_env.name,
+    resourceGroupName: resourceGroupName,
+    properties: {
+        azureFile: {
+            accountName: storageAccountName,
+            shareName: strapiAppFilesStorage.name, // "strapi-app-files"
+            accessMode: "ReadWrite",
+            accountKey: storageAccountKey,
+        },
+    },
+}, { protect: false, dependsOn: [strapiAppFilesStorage] });
+
+// Create dedicated storage for SuiteCRM (also in marketingstacksa)
+const suitecrmStorage = new azure_app.ManagedEnvironmentsStorage("suitecrm-app-files-storage", {
+    environmentName: marketing_env.name,
+    resourceGroupName: resourceGroupName,
+    properties: {
+        azureFile: {
+            accountName: storageAccountName,
+            shareName: suiteCrmAppFilesStorage.name, // e.g. "suitecrm-app-files"
+            accessMode: "ReadWrite",
+            accountKey: storageAccountKey,
+        },
+    },
+}, { protect: false, dependsOn: [suiteCrmAppFilesStorage] });
+
+// Create dedicated storage for Jumpbox
+const jumpboxStorage = new azure_app.ManagedEnvironmentsStorage("jumpbox-files-storage", {
+    environmentName: marketing_env.name,
+    resourceGroupName: resourceGroupName,
+    properties: {
+        azureFile: {
+            accountName: storageAccountName,
+            shareName: jumpboxFilesStorage.name, // "jumpbox-files"
+            accessMode: "ReadWrite",
+            accountKey: storageAccountKey,
+        },
+    },
+}, { protect: false, dependsOn: [jumpboxFilesStorage] });
+
+    break;
+  }
+  case "setup-apps": {
+    // Only deploy container apps, DNS, certs, jumpbox
+    // Deploy the Mautic Nginx App
+export const mauticNginxApp = mauticNginx({
+    env: appEnv,
+    image: getImageName("marketing-nginx"),
+    registryUrl: registryUrl,
+    registryUsername: acrUsername,
+    registryPassword: acrPassword,
+    managedEnvironmentId: marketing_env.id,
+    storageName: pulumi.output("mautic-app-files-storage"),
+    suiteCrmStorageName: pulumi.output("suitecrm-app-files-storage"),
+    dbHost: dbHost,
+    dbPort: dbPort,
+    dbName: dbName,
+    resourceGroupName: resourceGroupName,
+    createSubdomains: createSubdomains, // Set to false for initial deployment
+    // azureFunctionUrl: azureFunctionUrl, 
+});
+
+const siteFQDN = mauticNginxApp.configuration.apply(fqdn => fqdn?.ingress?.fqdn ?? "localhost");
+const nginxCvid = mauticNginxApp.customDomainVerificationId.apply(cvid => cvid);
+
+// Deploy the Mautic Web App
+export const mauticWebApp = mauticWeb({
+    env: appEnv,
+    image: getImageName("marketing-mautic-app"),
+    registryUrl: registryUrl,
+    registryUsername: acrUsername,
+    registryPassword: acrPassword,
+    managedEnvironmentId: marketing_env.id,
+    storageName: pulumi.output("mautic-app-files-storage"),
+    dbHost: dbHost,
+    dbPort: dbPort,
+    dbName: dbName,
+    dbUser: dbUser,
+    dbPassword: dbPassword,
+    appSecret: appSecret,
+    resourceGroupName: resourceGroupName, 
+    siteFQDN: siteFQDN,
+    siteUrl: pulumi.interpolate`https://${mapSubdomain}.${domain}/`,
+});
+
+// Deploy the Strapi App using the dedicated strapi storage mount
+export const deployedStrapiApp = strapiApp({
+    env: appEnv,
+    image: getImageName("marketing-strapi-app"),
+    registryUrl: registryUrl,
+    registryUsername: acrUsername,
+    registryPassword: acrPassword,
+    managedEnvironmentId: marketing_env.id,
+    storageName: pulumi.output("strapi-app-files-storage"),
+    dbHost: dbHost,
+    dbPort: dbPort,
+    dbName: strapiDbName,
+    dbUser: dbUser,
+    dbPassword: dbPassword,
+    dbClient: config.require("dbClient"),
+    jwtSecret: config.require("jwtSecret"),
+    adminJwtSecret: config.require("adminJwtSecret"),
+    appKeys: config.require("appKeys"),
+    nodeEnv: config.require("nodeEnv"),
+    resourceGroupName: resourceGroupName,
+    apiToken: config.require("apiToken"),
+    transferTokenSalt: config.require("transferTokenSalt"),
+    cmsUrl: pulumi.interpolate`https://${cmsSubdomain}.${domain}/`,
+});
+
+
+// Deploy the suitecrm App
+export const deployedSuitecrmApp = suitecrmApp({
+    env: appEnv,
+    appSecret: appSecret,
+    siteFQDN: siteFQDN,
+    image: getImageName("marketing-suitecrm-app"),
+    registryUrl: registryUrl,
+    registryUsername: acrUsername,
+    registryPassword: acrPassword,
+    managedEnvironmentId: marketing_env.id,
+    storageName: pulumi.output("suitecrm-app-files-storage"),
+    dbHost: dbHost,
+    dbPort: dbPort,
+    dbName: suitecrmDbName,
+    dbUser: dbUser,
+    dbPassword: dbPassword,
+    dbType: dbType,
+    dbVersion: dbVersion,
+    dbCharset: dbCharset,
+    resourceGroupName: resourceGroupName,
+    siteUrl: pulumi.interpolate`https://${crmSubdomain}.${domain}/`,
+    crmSubdomain: crmSubdomain,
+    domain: domain,
+});
+
+
+export const cloudflareDNSentries = BoolSubdomains ? setupDns({
+    domain: domain,
+    cmsSubdomain: cmsSubdomain,
+    crmSubdomain: crmSubdomain,
+    mapSubdomain: mapSubdomain,
+    siteFQDN: siteFQDN,
+    nginxCvid: nginxCvid,
+    mauticNginxApp: mauticNginxApp,
+    strapiApp: deployedStrapiApp,
+    strapiFQDN: deployedStrapiApp.configuration.apply(fqdn => fqdn?.ingress?.fqdn ?? "localhost"),
+    suiteCrmApp: deployedSuitecrmApp,
+    suiteCrmFQDN: deployedSuitecrmApp.configuration.apply(fqdn => fqdn?.ingress?.fqdn ?? "localhost"),
+  
+}) : undefined ; // Set to undefined if BoolSubdomains is false
+
+// Update mauticNginxApp to use the cloudflareDNSentries as the customDomains
+export const customDomains = nginxCerts(mauticNginxApp, deployedStrapiApp, marketing_env, cloudflareDNSentries);
+
+// Deploy the Jumpbox container app
+export const jumpboxApp = jumpbox({
+    env: appEnv,
+    managedEnvironmentId: marketing_env.id,
+    storageName: pulumi.output("jumpbox-files-storage"),
+    dbHost: dbHost,
+    dbPort: dbPort,
+    resourceGroupName: resourceGroupName,
+});
+    break;
+  }
+  default: {
+    // If not running in CI, or no GITHUB_JOB set, run everything (for local/dev)
+    // Create storage configuration in the managed environment for Mautic (uses marketingstacksa)
+const mauticStorage = new azure_app.ManagedEnvironmentsStorage("mautic-app-files-storage", {
+    environmentName: marketing_env.name,
+    resourceGroupName: resourceGroupName,
+    properties: {
+        azureFile: {
+            accountName: storageAccountName,
+            shareName: mauticAppFilesStorage.name, // e.g. "mautic-app-files"
+            accessMode: "ReadWrite",
+            accountKey: storageAccountKey,
+        },
+    }, 
+}, {
+    protect: false,
+    dependsOn: [mauticAppFilesStorage],
+});
+
+// Create dedicated storage for Strapi
+const strapiStorage = new azure_app.ManagedEnvironmentsStorage("strapi-app-files-storage", {
+    environmentName: marketing_env.name,
+    resourceGroupName: resourceGroupName,
+    properties: {
+        azureFile: {
+            accountName: storageAccountName,
+            shareName: strapiAppFilesStorage.name, // "strapi-app-files"
+            accessMode: "ReadWrite",
+            accountKey: storageAccountKey,
+        },
+    },
+}, { protect: false, dependsOn: [strapiAppFilesStorage] });
+
+// Create dedicated storage for SuiteCRM (also in marketingstacksa)
+const suitecrmStorage = new azure_app.ManagedEnvironmentsStorage("suitecrm-app-files-storage", {
+    environmentName: marketing_env.name,
+    resourceGroupName: resourceGroupName,
+    properties: {
+        azureFile: {
+            accountName: storageAccountName,
+            shareName: suiteCrmAppFilesStorage.name, // e.g. "suitecrm-app-files"
+            accessMode: "ReadWrite",
+            accountKey: storageAccountKey,
+        },
+    },
+}, { protect: false, dependsOn: [suiteCrmAppFilesStorage] });
+
+// Create dedicated storage for Jumpbox
+const jumpboxStorage = new azure_app.ManagedEnvironmentsStorage("jumpbox-files-storage", {
+    environmentName: marketing_env.name,
+    resourceGroupName: resourceGroupName,
+    properties: {
+        azureFile: {
+            accountName: storageAccountName,
+            shareName: jumpboxFilesStorage.name, // "jumpbox-files"
+            accessMode: "ReadWrite",
+            accountKey: storageAccountKey,
+        },
+    },
+}, { protect: false, dependsOn: [jumpboxFilesStorage] });
+
+// Function to get the image name from the registry
+function getImageName(imageName: string): pulumi.Output<string> {
+    return registryUrl.apply(url => `${url}/${imageName}:${imageTag}`);
+}
+
 // Deploy the Mautic Nginx App
 export const mauticNginxApp = mauticNginx({
     env: appEnv,
@@ -238,5 +507,8 @@ export const jumpboxApp = jumpbox({
     dbPort: dbPort,
     resourceGroupName: resourceGroupName,
 });
+    break;
+  }
+}
 
 
