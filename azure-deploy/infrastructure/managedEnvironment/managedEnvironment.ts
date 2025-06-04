fa564b424ea232402b1c70622b5aaa8ecf9af2eb
Adam Wheeler <ajwtech@gmail.com>
2025-06-03 20:22:55 -0400

    fix: refactor Log Analytics output retrieval to use StackReference for resource group name

--- azure-deploy/infrastructure/managedEnvironment/managedEnvironment.ts
+++ azure-deploy/infrastructure/managedEnvironment/managedEnvironment.ts
@@ -1,15 +1,15 @@
 import * as pulumi from "@pulumi/pulumi";
 import { v20241002preview as azure_app } from "@pulumi/azure-native/app";
 import { subnet } from "../networking/subnet";
+import { acr } from "../../stackRefs";
 
 const config = new pulumi.Config();
-const resourceGroupName = config.require("resourceGroupName");
 const location = config.require("location");
 
 // Use StackReference to get Log Analytics outputs from acrStack
-const acrStack = new pulumi.StackReference("ajwtech/scouten/acrStack");
-const logAnalyticsCustomerId = acrStack.getOutput("logAnalyticsCustomerId");
-const logAnalyticsSharedKey = acrStack.getOutput("logAnalyticsSharedKey");
+const resourceGroupName = acr.getOutput("resourceGroupName")
+const logAnalyticsCustomerId = acr.getOutput("logAnalyticsCustomerId");
+const logAnalyticsSharedKey = acr.getOutput("logAnalyticsSharedKey");
 
 export const marketing_env = new azure_app.ManagedEnvironment("marketing-env", {
     environmentName: "marketing-env",
